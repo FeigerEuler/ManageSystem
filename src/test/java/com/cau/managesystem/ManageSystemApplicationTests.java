677d27d21305fa687eca50f231789650057792b0
mhf <804941144@qq.com>
2022-04-26 21:26:19 +0800

    fist commit

---
+++ src/test/java/com/cau/managesystem/ManageSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cau.managesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world");
+    }
+
+}
